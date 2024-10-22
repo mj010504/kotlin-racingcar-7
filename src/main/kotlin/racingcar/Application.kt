77eba25ad35b031757f7da1c044819e78f0fa0bc
choiminjun <mj010504@naver.com>
2024-10-22 22:40:20 +0900

    feat: 자동차 입력받기

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val str = Console.readLine()
+    val carNameList = str.split(',')
+    val carDistanceList : MutableList<Int> = MutableList(carNameList.size) {0}
+
+
 }
