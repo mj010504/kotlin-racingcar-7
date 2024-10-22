a4b1fa7ffd338f21c13511abab6845c04b4f5180
choiminjun <mj010504@naver.com>
2024-10-23 00:22:57 +0900

    feat: 올바르지 않은 시도 횟수 입력받을 시 예외처리

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
 enum class ErrorMessages(val errorMessage: String) {
     INVALID_CAR_NAME("올바르지 않은 자동차 이름입니다. 자동차 이름은 반드시 알파벳 문자로 시작해야 하며, 뒤에 선택적으로 숫자를 포함할 수 있습니다"),
     INVALID_CAR_NAME_LENGTH("자동차 이름이 허용 길이를 초과했습니다. 자동차 이름은 5자 이하만 가능합니다."),
+    INVALID_MOVE_COUNT("올바르지 않은 시도 횟수입니다.")
 }
 
 fun main() {
@@ -17,12 +18,12 @@ fun main() {
         isValidCarName(carName)
     }
 
-    val carDistanceList = MutableList(carNameList.size) {0}
-
     println("시도할 횟수는 몇 회인가요?")
-    val moveCount  = Console.readLine().toInt()
+    val input = Console.readLine()
+    isValidMoveCount(input)
+    val moveCount = input.toInt()
 
-
+    val carDistanceList = MutableList(carNameList.size) {0}
     for(i in 0 until moveCount) {
         for(carIdx in 0 until carDistanceList.size) {
             moveCar(carIdx, carDistanceList)
@@ -49,9 +50,14 @@ fun moveCar(carIdx : Int, carDistanceList : MutableList<Int>) {
 
 fun isValidCarName(carName : String) {
     if(carName.length > 5) throwInvalidStringException(ErrorMessages.INVALID_CAR_NAME_LENGTH.errorMessage)
+    // 자동차 이름은 문자열 또는 문자열 + 숫자의 형태만 가능하다.
     val regex = Regex("^[A-Za-z]+[0-9]*$")
     if(!regex.matches(carName)) throwInvalidStringException(ErrorMessages.INVALID_CAR_NAME.errorMessage)
+}
 
+fun isValidMoveCount(input : String) {
+    val regex = Regex("^\\d+$")
+    if(!regex.matches(input)) throwInvalidStringException(ErrorMessages.INVALID_MOVE_COUNT.errorMessage)
 }
 
 private fun throwInvalidStringException(message : String) {
